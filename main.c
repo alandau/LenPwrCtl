15a01ee30b41b4d82ce56c749585e507adb1d644
Alex Landau <landau.alex@gmail.com>
2021-10-02 20:01:12 -0700

    Add project files.

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <Windows.h>
+#include "resource.h"
+
+INT_PTR CALLBACK MainDlgProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg) {
+	case WM_INITDIALOG:
+		return FALSE;
+	case WM_CLOSE:
+		EndDialog(hDlg, 0);
+		return TRUE;
+	}
+	return FALSE;
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd)
+{
+	DialogBox(hInstance, MAKEINTRESOURCE(IDD_MAIN_DIALOG), NULL, MainDlgProc);
+	return 0;
+}
